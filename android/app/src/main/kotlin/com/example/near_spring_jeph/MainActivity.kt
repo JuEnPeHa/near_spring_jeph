e28a86125624a18377b981721bbd80d558c7b206
JuEnPeHa <67208274+JuEnPeHa@users.noreply.github.com>
2022-04-17 19:31:20 -0500

    near splash starting repo

---
+++ android/app/src/main/kotlin/com/example/near_spring_jeph/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.near_spring_jeph
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/rpcFun.dart
@@ -0,0 +1,5 @@
+import 'dart:math';
+
+var rng = Random();
+
+Future<dynamic> rpcFunction()
